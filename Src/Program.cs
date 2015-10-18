093aea0d58ec03524cb0cfe7ba6e1bae6d36ae63
rstarkov <sourcecode@roman.st>
2015-10-18 22:52:03 +0100

    Stats on wards placed.

--- Src/Program.cs
+++ Src/Program.cs
@@ -323,10 +323,10 @@ namespace LeagueGenMatchHistory
                 return new TABLE { class_ = "ra stats" }._(
                     new TR(new TH(label) { rowspan = 2 }, new TH("Games") { rowspan = 2 }, new TH("Wins") { rowspan = 2 }, new TH("Losses") { rowspan = 2 }, new TH("Win%") { rowspan = 2 },
                         new TH("Kills/deaths/assists") { colspan = 6 }, new TH("Dmg to champs") { colspan = 2 }, new TH("Healing") { colspan = 2 },
-                        new TH("CS @ 10m") { colspan = 2 }, new TH("Gold @ 10m") { colspan = 2 }, new TH("Multikills every") { colspan = 4 }),
+                        new TH("CS @ 10m") { colspan = 2 }, new TH("Gold @ 10m") { colspan = 2 }, new TH("Multikills every") { colspan = 4 }, new TH("Wards") { colspan = 2 }),
                     new TR(
                         new TH("Avg/30m") { colspan = 3 }, new TH("Max") { colspan = 3 }, new TH("Avg/30m"), new TH("Max"), new TH("Avg/30m"), new TH("Max"),
-                        new TH("Avg"), new TH("Max"), new TH("Avg"), new TH("Max"), new TH("5x"), new TH("4x+"), new TH("3x+"), new TH("2x+")),
+                        new TH("Avg"), new TH("Max"), new TH("Avg"), new TH("Max"), new TH("5x"), new TH("4x+"), new TH("3x+"), new TH("2x+"), new TH("Avg/30m"), new TH("Max")),
                     set.OrderByDescending(g => g.Count()).Select(g => new TR(
                         new TD(g.Key) { class_ = "la" },
                         new TD(g.Count()),
@@ -350,7 +350,9 @@ namespace LeagueGenMatchHistory
                         new TD(fmtOrInf(g.Count() / (double) g.Count(p => p.LargestMultiKill >= 5))),
                         new TD(fmtOrInf(g.Count() / (double) g.Count(p => p.LargestMultiKill >= 4))),
                         new TD(fmtOrInf(g.Count() / (double) g.Count(p => p.LargestMultiKill >= 3))),
-                        new TD(fmtOrInf(g.Count() / (double) g.Count(p => p.LargestMultiKill >= 2)))
+                        new TD(fmtOrInf(g.Count() / (double) g.Count(p => p.LargestMultiKill >= 2))),
+                        new TD("{0:0.0}".Fmt(g.Average(p => p.WardsPlaced / p.Game.Duration.TotalMinutes * 30))),
+                        new TD(getGameValueAndLink(g.MaxElement(p => p.WardsPlaced), p => p.WardsPlaced.ToString("0")))
                     ))
                 );
             });
@@ -585,6 +587,7 @@ namespace LeagueGenMatchHistory
                 pp.TotalHeal = stats["totalHeal"].GetInt();
                 pp.TotalDamageTaken = stats["totalDamageTaken"].GetInt();
                 pp.LargestMultiKill = stats["largestMultiKill"].GetInt();
+                pp.WardsPlaced = stats.ContainsKey("wardsPlaced") ? stats["wardsPlaced"].GetInt() : 0;
                 var timeline = p["timeline"].GetDict();
                 if (Duration > TimeSpan.FromMinutes(10))
                 {
@@ -701,6 +704,7 @@ namespace LeagueGenMatchHistory
         public int Kills, Deaths, Assists;
         public int DamageToChampions, TotalHeal, TotalDamageTaken;
         public int LargestMultiKill;
+        public int WardsPlaced;
         public int CreepsAt10, CreepsAt20, CreepsAt30;
         public int GoldAt10, GoldAt20, GoldAt30;
 
