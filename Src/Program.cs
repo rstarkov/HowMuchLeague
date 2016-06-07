826be3d0da37f1fcb980de830da2612567e2e616
rstarkov <sourcecode@roman.st>
2016-06-07 23:54:00 +0100

    Bugfix for renamed summoners. Use RT.Projects as nuget packages.

--- Src/Program.cs
+++ Src/Program.cs
@@ -140,7 +140,7 @@ namespace LeagueGenMatchHistory
                 {
                     var data = resp.Expect(HttpStatusCode.OK).DataString;
                     var tryJson = JsonDict.Parse(data);
-                    Ut.Assert(tryJson["participantIdentities"].GetList().Any(l => l["player"]["summonerName"].GetString() == Summoner.Name)); // a bit redundant, but makes sure we don't save this if something went wrong
+                    Ut.Assert(tryJson["participantIdentities"].GetList().Any(l => Summoner.PastNames.Contains(l["player"]["summonerName"].GetString()))); // a bit redundant, but makes sure we don't save this if something went wrong
                     Directory.CreateDirectory(Path.GetDirectoryName(path));
                     File.WriteAllText(path, data);
                 }
