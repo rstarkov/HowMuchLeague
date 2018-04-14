8e5785ad95e2fb3917ba805962cb5ad7f46ce1db
rstarkov <sourcecode@roman.st>
2018-04-14 18:04:16 +0100

    Faster: total stats generation from 5m30s to 4m18s

--- LeagueOfStats.OneForAllStats/StatsGen.cs
+++ LeagueOfStats.OneForAllStats/StatsGen.cs
@@ -81,49 +81,59 @@ namespace LeagueOfStats.OneForAllStats
             writeAllLines($"{prefix}-match-ids.txt", matches.Select(m => m.MatchId).Order());
 
             // All possible matchup stats
-            var grps = newDict(new { ch1 = "", ch2 = "" }, new List<Match>(), _ => new List<Match>());
-            foreach (var m in matches)
-                grps[new { ch1 = m.Champion1, ch2 = m.Champion2 }].Add(m);
-            var empty = new List<Match>();
-            var statsMatchupsAll = champions.SelectMany(ch1 => champions.Select(ch2 => new { ch1, ch2 })).Where(key => key.ch1.CompareTo(key.ch2) <= 0).Select(key =>
             {
-                if (!grps.ContainsKey(key))
-                    return new { Champion1 = key.ch1, Champion2 = key.ch2, Count = 0, WinRate = 0.5, Lower95 = 0.0, Upper95 = 1.0, Lower67 = 0.0, Upper67 = 1.0 };
-                var n = grps[key].Count;
-                if (key.ch1 == key.ch2)
-                    return new { Champion1 = key.ch1, Champion2 = key.ch2, Count = n, WinRate = 0.5, Lower95 = 0.5, Upper95 = 0.5, Lower67 = 0.5, Upper67 = 0.5 };
-                var p = winrate(grps[key], key.ch1);
-                var conf95 = getWilson(p, n, 1.96);
-                var conf67 = getWilson(p, n, 0.97);
-                return new { Champion1 = key.ch1, Champion2 = key.ch2, Count = n, WinRate = p, Lower95 = conf95.lower, Upper95 = conf95.upper, Lower67 = conf67.lower, Upper67 = conf67.upper };
-            }).ToList();
-            statsMatchupsAll = statsMatchupsAll.Concat(statsMatchupsAll
-                    .Where(m => m.Champion1 != m.Champion2)
-                    .Select(m => new { Champion1 = m.Champion2, Champion2 = m.Champion1, m.Count, WinRate = 1 - m.WinRate, Lower95 = 1 - m.Upper95, Upper95 = 1 - m.Lower95, Lower67 = 1 - m.Upper67, Upper67 = 1 - m.Lower67 })
-                ).OrderBy(m => m.Champion1).ThenBy(m => m.Champion2).ToList();
-            writeAllLines($"{prefix}-matchupsall.csv", statsMatchupsAll.Select(l => $"{l.Champion1},{l.Champion2},{l.Count},{l.WinRate},{l.Lower95},{l.Upper95},{l.Lower67},{l.Upper67}"));
+                var grps = newDict(new { ch1 = "", ch2 = "" }, new List<Match>(), _ => new List<Match>());
+                foreach (var m in matches)
+                    grps[new { ch1 = m.Champion1, ch2 = m.Champion2 }].Add(m);
+                var empty = new List<Match>();
+                var statsMatchupsAll = champions.SelectMany(ch1 => champions.Select(ch2 => new { ch1, ch2 })).Where(key => key.ch1.CompareTo(key.ch2) <= 0).Select(key =>
+                {
+                    if (!grps.ContainsKey(key))
+                        return new { Champion1 = key.ch1, Champion2 = key.ch2, Count = 0, WinRate = 0.5, Lower95 = 0.0, Upper95 = 1.0, Lower67 = 0.0, Upper67 = 1.0 };
+                    var n = grps[key].Count;
+                    if (key.ch1 == key.ch2)
+                        return new { Champion1 = key.ch1, Champion2 = key.ch2, Count = n, WinRate = 0.5, Lower95 = 0.5, Upper95 = 0.5, Lower67 = 0.5, Upper67 = 0.5 };
+                    var p = winrate(grps[key], key.ch1);
+                    var conf95 = getWilson(p, n, 1.96);
+                    var conf67 = getWilson(p, n, 0.97);
+                    return new { Champion1 = key.ch1, Champion2 = key.ch2, Count = n, WinRate = p, Lower95 = conf95.lower, Upper95 = conf95.upper, Lower67 = conf67.lower, Upper67 = conf67.upper };
+                }).ToList();
+                statsMatchupsAll = statsMatchupsAll.Concat(statsMatchupsAll
+                        .Where(m => m.Champion1 != m.Champion2)
+                        .Select(m => new { Champion1 = m.Champion2, Champion2 = m.Champion1, m.Count, WinRate = 1 - m.WinRate, Lower95 = 1 - m.Upper95, Upper95 = 1 - m.Lower95, Lower67 = 1 - m.Upper67, Upper67 = 1 - m.Lower67 })
+                    ).OrderBy(m => m.Champion1).ThenBy(m => m.Champion2).ToList();
+                writeAllLines($"{prefix}-matchupsall.csv", statsMatchupsAll.Select(l => $"{l.Champion1},{l.Champion2},{l.Count},{l.WinRate},{l.Lower95},{l.Upper95},{l.Lower67},{l.Upper67}"));
+            }
 
             // Champion stats at champ select stage (unknown enemy)
-            var statsChampSelect = champions.Select(champion =>
             {
-                var grp = matchesWith(matches, champion).ToList();
-                var n = grp.Count;
-                var p = winrate(grp, champion);
-                var conf95 = getWilson(p, n, 1.96);
-                // Bans
-                var remaining = grp;
-                var bans = new string[5];
-                var banWR = new double[5];
-                for (int i = 0; i < 5; i++)
+                var grps = new AutoDictionary<string, List<Match>>(_ => new List<Match>());
+                foreach (var m in matches)
                 {
-                    var banResult = champions.Where(ban => ban != champion).Select(ban => new { ban, wr = winrate(matchesWithout(remaining, ban), champion) }).MaxElement(x => x.wr);
-                    bans[i] = banResult.ban;
-                    banWR[i] = banResult.wr;
-                    remaining = matchesWithout(remaining, bans[i]).ToList();
+                    grps[m.Champion1].Add(m);
+                    if (m.Champion1 != m.Champion2)
+                        grps[m.Champion2].Add(m);
                 }
-                return new { champion, Count = n, WinRate = p, Lower95 = conf95.lower, Upper95 = conf95.upper, bans, banWR };
-            }).OrderBy(r => r.champion).ToList();
-            writeAllLines($"{prefix}-champselect.csv", statsChampSelect.Select(l => $"{l.champion},{l.Count},{l.WinRate},{l.Lower95},{l.Upper95},{l.bans[0]},{l.banWR[0]},{l.bans[1]},{l.banWR[1]},{l.bans[2]},{l.banWR[2]},{l.bans[3]},{l.banWR[3]},{l.bans[4]},{l.banWR[4]}"));
+                var statsChampSelect = champions.Select(champion =>
+                {
+                    var n = grps[champion].Count;
+                    var p = winrate(grps[champion], champion);
+                    var conf95 = getWilson(p, n, 1.96);
+                    // Bans
+                    var remaining = grps[champion]; // bans calculation is destructive as we won't need the original list anyway
+                    var bans = new string[5];
+                    var banWR = new double[5];
+                    for (int i = 0; i < 5; i++)
+                    {
+                        var banResult = champions.Where(ban => ban != champion).Select(ban => new { ban, wr = winrate(matchesWithout(remaining, ban), champion) }).MaxElement(x => x.wr);
+                        bans[i] = banResult.ban;
+                        banWR[i] = banResult.wr;
+                        remaining = matchesWithout(remaining, bans[i]).ToList();
+                    }
+                    return new { champion, Count = n, WinRate = p, Lower95 = conf95.lower, Upper95 = conf95.upper, bans, banWR };
+                }).OrderBy(r => r.champion).ToList();
+                writeAllLines($"{prefix}-champselect.csv", statsChampSelect.Select(l => $"{l.champion},{l.Count},{l.WinRate},{l.Lower95},{l.Upper95},{l.bans[0]},{l.banWR[0]},{l.bans[1]},{l.banWR[1]},{l.bans[2]},{l.banWR[2]},{l.bans[3]},{l.banWR[3]},{l.bans[4]},{l.banWR[4]}"));
+            }
         }
 
         private static AutoDictionary<TKey, TValue> newDict<TKey, TValue>(TKey _, TValue __, Func<TKey, TValue> init)
@@ -165,11 +175,6 @@ namespace LeagueOfStats.OneForAllStats
         private static double winrate(IEnumerable<Match> matches, string champion)
         {
             return matches.Sum(m => m.Winner == champion ? (m.Champion1 == m.Champion2 ? 0.5 : 1) : 0) / matches.Count();
-        }
-
-        private static IEnumerable<Match> matchesWith(IEnumerable<Match> matches, string champion)
-        {
-            return matches.Where(m => m.Champion1 == champion || m.Champion2 == champion);
         }
 
         private static IEnumerable<Match> matchesWithout(IEnumerable<Match> matches, string champion)
